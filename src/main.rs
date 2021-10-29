44131b471750db9be2c25f252907ec0536eba470
Krouton <me+git@tokinia.me>
2021-10-30 01:16:13 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 use std::collections::HashMap;
 
-use p9_asm::text;
-
-// run {
-// let v: int = 1;
-// return v;
-// }
-
-// TEXT は通常 2 つ引数を取り, 第１引数に名前, 第 2 引数にサイズを指定できます. 第 1 引数の 名前には CALL 命令などの対象として利用するシンボルを指定します.
-// TEXT main·run(SB), $0-16
-// MOVQ    $1, r0+16(SP)
-// MOVQ    r0+16, r0+8(SP)
-// RET
+use p9_asm::{operand, popq, pushq, text};
 
 #[derive(Debug, PartialEq)]
 struct FunctionBlock {
@@ -44,7 +33,7 @@ impl AnalyzedFunctionBlock {
     }
 
     fn analyze(fb: &FunctionBlock) -> (usize, HashMap<String, usize>) {
-        let mut stack_size = 0;
+        let mut stack_size = 8;
         let mut var_to_align = HashMap::new();
         for stmt in fb.instructions.iter() {
             match stmt {
@@ -55,9 +44,7 @@ impl AnalyzedFunctionBlock {
                 _ => {}
             }
         }
-
-        // +8 is for BP
-        (stack_size + 8, var_to_align)
+        (stack_size, var_to_align)
     }
 }
 
@@ -65,24 +52,16 @@ impl AnalyzedFunctionBlock {
     fn to_string(&self) -> String {
         let text = text!(run);
         let prologue = format!(
-            r#"SUBQ    ${}, SP
-MOVQ    BP, 8(SP)
-LEAQ    8(SP), BP
-"#,
-            self.stack_size
+            "{}{}{}",
+            format!("SUBQ ${}, SP\n", self.stack_size),
+            format!("MOVQ BP, {}(SP)\n", self.stack_size - 8),
+            format!("LEAQ {}(SP), BP\n", self.stack_size - 8)
         );
-
         let code = self.emit_fb();
 
-        let epilogue = format!(
-            r#"MOVQ    8(SP), BP
-ADDQ    ${}, SP
-RET
-"#,
-            self.stack_size
-        );
-
-        format!("{}{}{}{}", text, prologue, code, epilogue)
+        // let epilogue = format!("{}{}{}", "MOVQ BP, SP\n", popq!(BP), "RET\n");
+        // TODO: write epilogue
+        format!("{}{}{}", text, prologue, code)
     }
 
     fn emit_fb(&self) -> String {
@@ -104,38 +83,29 @@ enum Statement {
 impl Statement {
     fn emit(&self, stack_size: usize, var_to_align: &HashMap<String, usize>) -> String {
         match self {
-            Statement::Let(var_name, lit) => Self::emit_let(var_name, lit, var_to_align),
-            Statement::Return(lit) => match lit {
-                Expression::Integer(n) => format!("MOVQ ${} r0+{}(SP)\n", n, stack_size + 8),
-                Expression::Ident(i) => {
-                    let src_align = var_to_align.get(i).unwrap();
-                    format!(
-                        r#"MOVQ {}(SP), AX
-MOVQ AX, r0+{}(SP)
-"#,
-                        src_align,
-                        stack_size + 8
-                    )
-                }
-            },
-        }
-    }
-
-    fn emit_let(var_name: &str, lit: &Expression, var_to_align: &HashMap<String, usize>) -> String {
-        if let Some(align) = var_to_align.get(var_name) {
-            match lit {
-                Expression::Integer(n) => format!("MOVQ ${}, {}(SP)\n", n, align),
-                Expression::Ident(i) => {
-                    let src_align = var_to_align.get(i).unwrap();
-                    format!(
-                        r#"MOVQ {}(SP), AX
-MOVQ AX, {}(SP)"#,
-                        src_align, align
-                    )
+            Statement::Let(var_name, lit) => {
+                let expr_emitted = lit.emit(var_to_align);
+                let popq = popq!(AX);
+                if let Some(align) = var_to_align.get(var_name) {
+                    let alloc = format!("MOVQ AX, r0+{}(SP)\n", align);
+                    format!("{}{}{}", expr_emitted, popq, alloc)
+                } else {
+                    unreachable!()
                 }
             }
-        } else {
-            unreachable!()
+            Statement::Return(lit) => {
+                let emit_expr = lit.emit(var_to_align);
+                let popq = popq!(AX);
+                let alloc = format!("MOVQ AX, r0+{}(SP)\n", stack_size);
+                let epilogue = format!(
+                    "{}{}{}{}",
+                    format!("MOVQ AX, ret+8(FP)\n"),
+                    format!("MOVQ {}(SP), BP\n", stack_size - 8),
+                    format!("ADDQ ${}, SP\n", stack_size),
+                    format!("RET\n")
+                );
+                format!("{}{}{}{}", emit_expr, popq, alloc, epilogue)
+            }
         }
     }
 }
@@ -146,14 +116,32 @@ enum Expression {
     Ident(String),
 }
 
+impl Expression {
+    fn emit(&self, var_to_align: &HashMap<String, usize>) -> String {
+        match self {
+            Self::Integer(n) => pushq!(n),
+            Self::Ident(id) => {
+                if let Some(align) = var_to_align.get(id) {
+                    let align = format!("LEAQ {}+{}(SP), AX\n", id, align);
+                    format!("{}{}", align, pushq!(AX))
+                } else {
+                    unreachable!()
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let run_block = FunctionBlock::new(
         "run".to_string(),
         vec![
-            Statement::Let("v".to_string(), Expression::Integer(200)),
-            Statement::Let("x".to_string(), Expression::Integer(100)),
-            Statement::Return(Expression::Ident("x".to_string())),
+            Statement::Let("z".to_string(), Expression::Integer(100)),
+            Statement::Return(Expression::Ident("z".to_string())),
         ],
     );
-    println!("{}", AnalyzedFunctionBlock::new(run_block).to_string());
+    let afb = AnalyzedFunctionBlock::new(run_block);
+    println!("#include \"go_asm.h\"\n");
+    println!("{}", afb.to_string());
+    dbg!(afb);
 }
