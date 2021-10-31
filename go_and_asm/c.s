4d379d083a9a85933341d97fe4aba31e21dec0b3
Krouton <me+git@tokinia.me>
2021-10-31 09:30:58 +0900

    $ go tool compile -S -N -l c.go | trim-go-asm --fm --ra  > ./c.s

---
+++ go_and_asm/c.s
@@ -0,0 +1,14 @@
+TEXT	main·run(SB), 4, $16-16
+SUBQ	$16, SP
+MOVQ	BP, 8(SP)
+LEAQ	8(SP), BP
+FUNCDATA	$0, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
+FUNCDATA	$1, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
+MOVQ	$0, r1+32(SP)
+MOVQ	$100, y(SP)
+MOVQ	x+24(SP), AX
+ADDQ	$100, AX
+MOVQ	AX, r1+32(SP)
+MOVQ	8(SP), BP
+ADDQ	$16, SP
+RET
