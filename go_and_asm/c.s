e7a51e64ab4be3fe88990bc131cd4cd709bf77c9
Krouton <me+git@tokinia.me>
2021-10-31 10:27:43 +0900

    Ahhhhhh I've noticed!!!!

--- go_and_asm/c.s
+++ go_and_asm/c.s
@@ -4,7 +4,7 @@ MOVQ	BP, 8(SP)
 LEAQ	8(SP), BP
 FUNCDATA	$0, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
 FUNCDATA	$1, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
-MOVQ	$0, r1+32(SP)
+MOVQ	$0, r1+32(SP) // Initialize the Zero value of Go for virtual return variable(r1).
 MOVQ	$100, y(SP)
 MOVQ	x+24(SP), AX
 ADDQ	$100, AX
