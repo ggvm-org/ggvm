7fe45e429696ae05a6074c2e0fe318067fccf868
Krouton <me+git@tokinia.me>
2021-11-13 17:18:15 +0900

    Add add_inst, ret_inst.

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,6 +1,11 @@
 use nom::{
-    bytes::complete::tag, character::complete::alpha1, character::complete::char,
-    character::complete::multispace0 as sp, sequence::tuple, IResult,
+    bytes::complete::tag,
+    character::complete::alpha1,
+    character::complete::char,
+    character::complete::multispace0 as sp,
+    combinator::{map, value},
+    sequence::tuple,
+    IResult,
 };
 
 // func $add(%x int, %y int) int {
@@ -8,20 +13,55 @@ use nom::{
 // 	ret int %z;
 // }
 
+#[derive(Debug, PartialEq)]
 enum Operand<'a> {
     Var(&'a str),
 }
 
-fn typ(input: &str) -> IResult<&str, &str> {
-    let (input, typ) = tag("int")(input)?;
+#[derive(Debug, PartialEq, Clone)]
+enum Typ {
+    Int,
+}
+
+#[derive(Debug, PartialEq)]
+enum Instruction<'a> {
+    Add(Typ, Operand<'a>, Operand<'a>),
+    Ret(Typ, Operand<'a>),
+}
+
+fn typ(input: &str) -> IResult<&str, Typ> {
+    let (input, typ) = value(Typ::Int, tag("int"))(input)?;
     let (input, _) = sp(input)?;
     Ok((input, typ))
+}
+
+fn operand(input: &str) -> IResult<&str, Operand> {
+    map(var, Operand::Var)(input)
 }
 
 fn var(input: &str) -> IResult<&str, &str> {
     let (input, (_, var_literal)) = tuple((char('%'), alpha1))(input)?;
     let (input, _) = sp(input)?;
     Ok((input, var_literal))
+}
+
+fn add_inst(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("add")(input)?;
+    let (input, _) = sp(input)?;
+    let (input, typ) = typ(input)?;
+    let (input, left_op) = operand(input)?;
+    let (input, _) = char(',')(input)?;
+    let (input, _) = sp(input)?;
+    let (input, right_op) = operand(input)?;
+    Ok((input, Instruction::Add(typ, left_op, right_op)))
+}
+
+fn ret_inst(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("ret")(input)?;
+    let (input, _) = sp(input)?;
+    let (input, typ) = typ(input)?;
+    let (input, ret_val) = operand(input)?;
+    Ok((input, Instruction::Ret(typ, ret_val)))
 }
 
 #[test]
@@ -39,5 +79,26 @@ fn typ_test() {
     assert!(result.is_ok());
     let (rest, typ_lit) = result.unwrap();
     assert_eq!("%x", rest);
-    assert_eq!("int", typ_lit);
+    assert_eq!(Typ::Int, typ_lit);
+}
+
+#[test]
+fn add_inst_test() {
+    let result = add_inst("add int %x, %y");
+    assert!(result.is_ok());
+    let (rest, add_inst) = result.unwrap();
+    assert_eq!("", rest);
+    assert_eq!(
+        Instruction::Add(Typ::Int, Operand::Var("x"), Operand::Var("y")),
+        add_inst
+    );
+}
+
+#[test]
+fn ret_inst_test() {
+    let result = ret_inst("ret int %x");
+    assert!(result.is_ok());
+    let (rest, ret_inst) = result.unwrap();
+    assert_eq!("", rest);
+    assert_eq!(Instruction::Ret(Typ::Int, Operand::Var("x")), ret_inst);
 }
