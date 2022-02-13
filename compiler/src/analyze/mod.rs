3da65c711a742b092a267e912c64292cc5630a95
Krouton <me+git@tokinia.me>
2022-02-13 17:40:51 +0900

    WIP

--- compiler/src/analyze/mod.rs
+++ compiler/src/analyze/mod.rs
@@ -1,8 +1,15 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, ops::Deref};
 
-use crate::Operand;
+use crate::{Func, Operand};
 
 pub(crate) struct Environment<'a>(HashMap<Operand<'a>, usize>);
+
+impl<'a> Deref for Environment<'a> {
+    type Target = HashMap<Operand<'a>, usize>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 // func $add(%x int, %y int) int {
 //   %z = add int %x, %y
@@ -14,6 +21,19 @@ pub(crate) struct Environment<'a>(HashMap<Operand<'a>, usize>);
 pub(crate) struct AnalyzeResult<'a> {
     stacksize: usize,
     env: Environment<'a>,
+}
+
+impl<'a> AnalyzeResult<'a> {
+    fn new(stacksize: usize, env: Environment<'a>) -> Self {
+        Self { stacksize, env }
+    }
+}
+
+pub(crate) fn analyze(func: Func) -> AnalyzeResult {
+    let stacksize = 10000;
+    let mut env = Environment::new();
+
+    AnalyzeResult { stacksize, env }
 }
 
 impl Environment<'_> {
