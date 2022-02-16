47ffcf1443f814d7f407a8efa91e05f3eb46fd08
Krouton <me+git@tokinia.me>
2022-02-16 11:44:43 +0900

    Add Register and RegisterWithOffset

--- compiler/src/go_assembly/mod.rs
+++ compiler/src/go_assembly/mod.rs
@@ -12,6 +12,57 @@ pub(crate) enum GoAssemblyKind {
     Addq(Operand, Operand),
 }
 
+#[derive(Debug, PartialEq)]
+pub(crate) enum AsmOperand {
+    RegisterWithOffset(RegisterWithOffset),
+}
+
+impl ToString for AsmOperand {
+    fn to_string(&self) -> String {
+        match self {
+            AsmOperand::RegisterWithOffset(inner) => inner.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub(crate) struct RegisterWithOffset {
+    offset: usize,
+    register: Register,
+}
+
+impl ToString for RegisterWithOffset {
+    fn to_string(&self) -> String {
+        let offset = self.offset;
+        let register = self.register;
+        if offset == 0 {
+            register.to_string()
+        } else {
+            "{offset}(register)".to_string()
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub(crate) enum Register {
+    AX,
+    CX,
+    SP,
+    BP,
+}
+
+impl ToString for Register {
+    fn to_string(&self) -> String {
+        match self {
+            &Self::AX => "AX",
+            &Self::CX => "CX",
+            &Self::SP => "SP",
+            &Self::BP => "BP",
+        }
+        .to_string()
+    }
+}
+
 impl ToString for GoAssemblyKind {
     fn to_string(&self) -> String {
         match self {
