8f073c730ba64344e488499b43f534394b832b5e
Krouton <me+git@tokinia.me>
2022-02-25 20:15:49 +0900

    Should revert

--- tests/func_amd64.s
+++ tests/func_amd64.s
@@ -1,16 +1,17 @@
-TEXT    main·run(SB), 4, $32-16
-SUBQ    $32, SP
-MOVQ    BP, 24(SP)
-LEAQ    24(SP), BP
-MOVQ    $0, r1+48(SP)
-MOVQ    $100, (SP)
-PCDATA  $1, $0
-CALL    encoding/hex·DecodedLen(SB)
-MOVQ    8(SP), AX
-MOVQ    AX, autotmp_2+16(SP)
-MOVQ    x+40(SP), CX
-ADDQ    CX, AX
-MOVQ    AX, r1+48(SP)
-MOVQ    24(SP), BP
-ADDQ    $32, SP
+TEXT	main·run(SB), 4, $32-8
+SUBQ	$32, SP
+MOVQ	BP, 24(SP)
+
+LEAQ	24(SP), BP
+MOVQ	AX, main·x+40(SP)
+MOVQ	$10, main·r1+8(SP)
+MOVQ	main·x+40(SP), AX
+PCDATA	$1, $0
+CALL	main·fib(SB)
+MOVQ	AX, 16(SP)
+MOVQ	AX, main·r1+8(SP)
+
+MOVQ	24(SP), BP
+ADDQ	$32, SP
+NOP
 RET
