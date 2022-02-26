18ab852e061a4554d79da85bf1201d6478bcec9b
Krouton <me+git@tokinia.me>
2022-02-26 11:24:51 +0900

    Add new_goroutine_prologue.

--- compiler/src/go_assembly/mod.rs
+++ compiler/src/go_assembly/mod.rs
@@ -13,12 +13,38 @@ pub enum GoAssemblyKind {
     Call(AsmOperand),
     Addq(AsmOperand, AsmOperand),
     Ret,
+
+    // CMPQ	SP, 16(R14)
+    Cmpq(AsmOperand, AsmOperand),
+    // PCDATA	$0, $-2
+    PCData(AsmOperand, AsmOperand),
+    // JLS	epi
+    JLS(AsmOperand),
+    // epi:
+    Label(String),
+}
+
+impl GoAssembly {
+    fn new_goroutine_prologue() -> Self {
+        Self(vec![
+            GoAssemblyKind::Cmpq(
+                AsmOperand::Register(Register::SP),
+                AsmOperand::RegisterWithOffset(RegisterWithOffset {
+                    offset: 16,
+                    register: Register::R14,
+                }),
+            ),
+            GoAssemblyKind::PCData(AsmOperand::Int(0), AsmOperand::Int(-2)),
+            GoAssemblyKind::JLS(AsmOperand::Ident("epi".to_string())),
+            GoAssemblyKind::Label("body".to_string()),
+        ])
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum AsmOperand {
     Ident(String),
-    Int(usize),
+    Int(i64),
     RegisterWithOffset(RegisterWithOffset),
     Register(Register),
 }
@@ -62,6 +88,7 @@ pub enum Register {
     CX,
     SP,
     BP,
+    R14,
 }
 
 impl fmt::Display for Register {
@@ -71,6 +98,7 @@ impl fmt::Display for Register {
             &Self::CX => "CX",
             &Self::SP => "SP",
             &Self::BP => "BP",
+            &Self::R14 => "R14",
         };
         write!(f, "{s}")
     }
@@ -84,6 +112,10 @@ impl fmt::Display for GoAssemblyKind {
             Self::Call(AsmOperand::Ident(ident)) => format!("CALL    main·{ident}(SB)"),
             Self::Addq(left, right) => format!("ADDQ	{}, {}", left, right),
             Self::Movq(left, right) => format!("MOVQ	{}, {}", left, right),
+            Self::Cmpq(left, right) => format!("Cmpq	{}, {}", left, right),
+            Self::JLS(target) => format!("JLS	{}", target),
+            Self::PCData(left, right) => format!("PCDATA {}, {}", left, right),
+            Self::Label(label_name) => format!("{}:", label_name),
             _ => unimplemented!(),
         };
         write!(f, "{s}")
@@ -131,6 +163,8 @@ mod insta {
         };
     }
 
+    insta_test!(go_routine_prologue: GoAssembly::new_goroutine_prologue());
+
     insta_test!(
         go_assembly:
             GoAssembly(
@@ -172,7 +206,15 @@ mod insta {
         GoAssemblyKind::Addq(
             AsmOperand::Int(10000),
             AsmOperand::Register(SP)
-        )
+        ),
+        GoAssemblyKind::JLS(AsmOperand::Int(100)),
+        GoAssemblyKind::JLS(AsmOperand::Ident("a".to_string())),
+        GoAssemblyKind::Cmpq(AsmOperand::Register(SP), AsmOperand::RegisterWithOffset(RegisterWithOffset{
+            offset: 16,
+            register: R14
+        })),
+        GoAssemblyKind::PCData(AsmOperand::Int(1), AsmOperand::Int(2)),
+        GoAssemblyKind::Label("epi".to_string())
     );
 
     insta_test!(
