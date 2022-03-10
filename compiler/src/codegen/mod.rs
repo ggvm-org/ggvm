01994553fb281932ec5c9ee5418fcdd67d13a10d
Krouton <me+git@tokinia.me>
2022-03-10 18:23:48 +0900

    Update dependencies, then the directives! solves everything

--- compiler/src/codegen/mod.rs
+++ compiler/src/codegen/mod.rs
@@ -10,18 +10,20 @@ pub(crate) fn compile_func(func: AnalyzeResult) -> GoAssembly {
     let func = func.func;
     let package = "main".to_string();
     let name = func.name;
-    operand!(AX);
-    let mut asm = GoAssembly(vec![
-        Directive::Text { package, name },
-        SUBQ!(10000, SP),
-        MOVQ!(BP, 16=>SP),
-    ]);
+    let mut asm = GoAssembly(directives!(
+        TEXT main.run;
+        SUBQ [10000], [SP];
+        MOVQ [BP], [16(SP)];
+    ));
     let body = GoAssembly(func.stmts.into_iter().fold(vec![], |mut body, stmt| {
         body.append(&mut compile_stmt(stmt).0);
         body
     }));
 
-    let epilogue = GoAssembly(vec![MOVQ!(16=>SP, BP), ADDQ!(10000, SP)]);
+    let epilogue = GoAssembly(directives!(
+        MOVQ [16(SP)], [BP];
+        ADDQ [10000], [SP];
+    ));
     asm.extend(body.0);
     asm.extend(epilogue.0);
     asm
@@ -37,15 +39,10 @@ pub(crate) fn compile_stmt(stmt: super::Statement) -> GoAssembly {
 pub(crate) fn compile_inst(inst: Instruction) -> GoAssembly {
     GoAssembly(match inst {
         Instruction::Add(typ, a, b) => unimplemented!(),
-        Instruction::Ret(typ, a) => vec![directive!(RET)],
-        Instruction::Call(Operand::Var(var)) => {
-            vec![
-                Directive::Call {
-                    package: "main".to_string(),
-                    name: var,
-                },
-                MOVQ!(AX, 8 => SP),
-            ]
-        }
+        Instruction::Ret(typ, a) => directives!(RET;),
+        Instruction::Call(Operand::Var(var)) => directives!(
+            CALL {"main"}.{var};
+            MOVQ [AX], [8(SP)];
+        ),
     })
 }
